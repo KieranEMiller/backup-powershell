5a8c80077cd1b85790be33d2014f5b40fda6efdd
kieranemiller <kieranemiller@gmail.com>
2022-07-03 09:00:27 -0400

    correct sample source and destination paths: rename source2 to dest1

--- backup.ps1
+++ backup.ps1
@@ -1,8 +1,8 @@
 Import-Module ./BackupFileSystem.psm1
 
 Copy-DirTreeRecursively `
-    -SourcePath "c:\temp\source1" `
-    -DestinationPath "c:\temp\source2" `
+    -SourcePath "C:\temp\source1" `
+    -DestinationPath "C:\temp\dest1" `
     -LogPath "backup.log"
 
 Read-Host "done.  press enter to close"
