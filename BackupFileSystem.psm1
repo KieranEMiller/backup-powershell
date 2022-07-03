17beb471f006baef13f45132dd4c69d0b067aa3f
kieranemiller <kieranemiller@gmail.com>
2022-07-03 09:44:08 -0400

    1) add validation to confirm existance of both source and destination 2) move local path calculation to a separate function 3) move validation and config display to a separate function

--- BackupFileSystem.psm1
+++ BackupFileSystem.psm1
@@ -9,56 +9,78 @@ function Copy-DirTreeRecursively {
 		[string]$LogPath = ""
     )
 
-	if([string]::IsNullOrEmpty($SourcePath)) {
-		throw "missing argument parameter sourcePath.  you must specify a valid source path to copy from"
-	}
-	
-	if([string]::IsNullOrEmpty($DestinationPath)) {
-		throw "missing argument parameter destPath.  you must specify a valid destination path to copy to"
-	}
-	Log $LogPath ("config source: {0}" -f $SourcePath)
-	Log $LogPath ("config destination: {0}" -f $DestinationPath)
-	Log $LogPath ("logging to: {0}" -f $LogPath)
-	
-	#optional: only look up files that were modified or created in the last X days
-	<#
-	$rootDir = Get-ChildItem $PATH_TARGET_BASE_DIR -Recurse | `
-		where { `
-			$_.LastWriteTime -ge (Get-Date).AddDays($LOOKBACK_PERIOD) `
-			-OR $_.CreationTime -ge (Get-Date).AddDays($LOOKBACK_PERIOD) `
-	}#>
-	
-	
+	#throws an exception when either path is invalid
+	ValidateAndDocumentInputParameters $SourcePath $DestinationPath $LogPath
+
 	$paths = `
-		Get-ChildItem $SourcePath -Recurse -ErrorVariable +FailedItems `
+		Get-ChildItem $SourcePath -Recurse -Force -ErrorVariable +FailedItems `
 		| Foreach-Object { `
 			CopyRelative $LogPath $_ $SourcePath $DestinationPath `
 		} 
 	
 	$FailedItems `
 		| Foreach-Object { `
-			Log $LogPath ("ERROR: failed to process`t{0}" -f $_.CategoryInfo) `
+			Log $LogPath ("ERROR: failed to process: {0}" -f $_.CategoryInfo) `
 		} 
 
+	#optional example: only look up files that were modified or created in the last X days
+	<#
+	$rootDir = Get-ChildItem $PATH_TARGET_BASE_DIR -Recurse | `
+		where { `
+			$_.LastWriteTime -ge (Get-Date).AddDays($LOOKBACK_PERIOD) `
+			-OR $_.CreationTime -ge (Get-Date).AddDays($LOOKBACK_PERIOD) `
+	}#>
 }
 Export-ModuleMember -Function Copy-DirTreeRecursively
 
 function CopyRelative($logPath, $path, $sourcePathRoot, $destinationPath)
 {
 	try {
-		#strip out the root portion of the path
-		#this will create a relative path preserving the folder
-		#structure of the original file when it is copied locally
-		$relativePath = $path.FullName.replace($sourcePathRoot, "")
-		$fullLocalPath = Join-Path $destinationPath $relativePath
+		$localPath = GetRelativePath $path $sourcePathRoot $destinationPath
 
-		Log $logPath ("copy `t{0}`tto`t{1}" -f $path.FullName, $fullLocalPath)
-		Copy-Item $path.FullName -Destination $fullLocalPath -Force
+		Log $logPath ("copy `t{0}`tto`t{1}" -f $path.FullName, $localPath)
+		Copy-Item $path.FullName -Destination $localPath -Force
 	}
 	catch [System.Exception] {
 		$errMsg = $_
 		Log $logPath ("ERROR: failed to copy {0}: {1}" -f $path.FullName, $errMsg)
 	}
+}
+
+function ValidateAndDocumentInputParameters($SourcePath, $DestinationPath, $LogPath)
+{
+	if(-Not(IsValidDirectory $SourcePath )) {
+		throw "missing or invalid argument parameter sourcePath.  you must specify a valid source path to copy from"
+	}
+	
+	if(-Not(IsValidDirectory $DestinationPath)) {
+		throw "missing or invalid argument parameter destPath.  you must specify a valid destination path to copy to"
+	}
+	Log $LogPath ("config source: {0}" -f $SourcePath)
+	Log $LogPath ("config destination: {0}" -f $DestinationPath)
+	Log $LogPath ("logging to: {0}" -f $LogPath)
+}
+
+function IsValidDirectory($path)
+{
+	if([string]::IsNullOrEmpty($path)) {
+		return $false
+	}
+	
+	if(-Not(Test-Path -Path $path)) {
+		return $false
+	}
+	
+	return $true
+}
+
+function GetRelativePath($path, $sourcePathRoot, $destinationPath) 
+{
+	#strip out the root portion of the path
+	#this will create a relative path preserving the folder
+	#structure of the original file when it is copied locally
+	$relativePath = $path.FullName.replace($sourcePathRoot, "")
+	return Join-Path $destinationPath $relativePath
 }
 
 function Log($path, $msg) 
