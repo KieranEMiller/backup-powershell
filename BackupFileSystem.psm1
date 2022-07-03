e4b20080d40934a15f9156104ea4cc68df68cfae
kieranemiller <kieranemiller@gmail.com>
2022-07-03 09:02:51 -0400

    implement backup as a series of piped commands with error handling.  relative path calculated for each item found

--- BackupFileSystem.psm1
+++ BackupFileSystem.psm1
@@ -1,5 +1,4 @@
 function Copy-DirTreeRecursively {
-	#[CmdletBinding()]
 	param(
 		[Parameter(Mandatory=$true, Position=0)]
 		[string]$SourcePath,
@@ -21,11 +20,48 @@ function Copy-DirTreeRecursively {
 	Log $LogPath ("config destination: {0}" -f $DestinationPath)
 	Log $LogPath ("logging to: {0}" -f $LogPath)
 	
+	#optional: only look up files that were modified or created in the last X days
+	<#
+	$rootDir = Get-ChildItem $PATH_TARGET_BASE_DIR -Recurse | `
+		where { `
+			$_.LastWriteTime -ge (Get-Date).AddDays($LOOKBACK_PERIOD) `
+			-OR $_.CreationTime -ge (Get-Date).AddDays($LOOKBACK_PERIOD) `
+	}#>
 	
+	
+	$paths = `
+		Get-ChildItem $SourcePath -Recurse -ErrorVariable +FailedItems `
+		| Foreach-Object { `
+			CopyRelative $LogPath $_ $SourcePath $DestinationPath `
+		} 
+	
+	$FailedItems `
+		| Foreach-Object { `
+			Log $LogPath ("ERROR: failed to process {0}" -f $_.CategoryInfo.LocationTarget) `
+		} 
 
 }
 Export-ModuleMember -Function Copy-DirTreeRecursively
 
+function CopyRelative($logPath, $path, $sourcePathRoot, $destinationPath)
+{
+	Log $logPath ("found {0} in source path {1} going to {2}" -f $path.FullName, $sourcePathRoot, $destinationPath)
+
+	try {
+		#strip out the root portion of the path
+		#this will create a relative path preserving the folder
+		#structure of the original file when it is copied locally
+		$relativePath = $path.FullName.replace($sourcePathRoot, "")
+		$fullLocalPath = Join-Path $destinationPath $relativePath
+
+		Log $logPath ("copy `t{0}`tto`t{1}" -f $path.FullName, $fullLocalPath)
+		Copy-Item $path.FullName -Destination $fullLocalPath -Force
+	}
+	catch [System.Exception] {
+		$errMsg = $_
+		Log $logPath ("ERROR: failed to copy {0}: {1}" -f $path.FullName, $errMsg)
+	}
+}
 
 function Log($path, $msg) 
 {
